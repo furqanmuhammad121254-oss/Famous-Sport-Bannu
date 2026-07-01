fe4f125d5b9e8ad471edb43ce7abb9494273cec0
Furqan Muhammad <furqanmuhammad121254@gmail.com>
2026-07-01 10:15:16 -0700

    check role change

--- Backend/middlewares/CheckRole.js
+++ Backend/middlewares/CheckRole.js
@@ -1,36 +1,13 @@
 
 
 
-// export default (...roles) => {
-//     return (req, res, next) => {
-//         if (!roles.includes(req.user.role)) {
-//             return res.status(400).json({ msg: 'Role is Required' });
-//         }
-//         console.log(roles, 'In role middleware');
-//         next()
-//     }
-
-// }
-
-
-const checkRole = (...roles) => {
-  return (req, res, next) => {
-    if (!req.user) {
-      return res.status(401).json({
-        success: false,
-        message: "Unauthorized",
-      });
+export default (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return res.status(400).json({ msg: 'Role is Required' });
+        }
+        console.log(roles, 'In role middleware');
+        next()
     }
 
-    if (!roles.includes(req.user.role)) {
-      return res.status(403).json({
-        success: false,
-        message: "Access denied",
-      });
-    }
-
-    next();
-  };
-};
-
-export default checkRole;
+}
